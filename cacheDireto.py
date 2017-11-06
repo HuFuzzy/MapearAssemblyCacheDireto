a4f0ee68bf45cd71ac5ae92f4f9982f31a205a57
HuFuzzy <rodrigo.rosa.005@acad.pucrs.br>
2017-11-06 13:10:03 -0200

    Commit

--- cacheDireto.py
+++ cacheDireto.py
@@ -1,8 +1,17 @@
+from linha import Linha
+from tag import Tag
+
+
 
 lines = [line.rstrip('\n') for line in open('ACESSOS/AcessoBinario.txt')]
 bitTag = input("BITS PARA TAG");
 bitLinha = input("BITS PARA LINHA");
 dado = input("BITS PARA PALAVRA");
+
+lin00 = Linha('00');
+lin01 = Linha('01');
+lin10 = Linha('10');
+lin11 = Linha('11');
 
 for x in range(0, len(lines)):
 
---
+++ tag.py
@@ -0,0 +1,4 @@
+class Tag:
+    def __init__(self, nrTag, palavra, nrPalavras):
+        self.nrTag = nrTag
+        self.palavra = palavra
